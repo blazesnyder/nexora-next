abfe51ec11a9e9b4ff778cc949a53e5c084de438
blazesnyder <156258051+blazesnyder@users.noreply.github.com>
2026-07-17 12:02:48 +0800

    fix: iframe-based ads + restore Social Bar scripts

--- src/components/ads/AdsterraSocialBar.tsx
+++ src/components/ads/AdsterraSocialBar.tsx
@@ -1,16 +1,20 @@
 "use client"
 
-import { usePathname } from "next/navigation"
 import Script from "next/script"
 
 export default function AdsterraSocialBar() {
-  const pathname = usePathname()
-  if (pathname.startsWith("/admin")) return null
-
   return (
-    <Script
-      src="https://pl30399277.effectivecpmnetwork.com/16/aa/0b/16aa0b9c9132774934d913c627295656.js"
-      strategy="afterInteractive"
-    />
+    <>
+      <Script
+        src="https://pl30399256.effectivecpmnetwork.com/fdff6bdc16ce487f09160f7bcf039002/invoke.js"
+        strategy="afterInteractive"
+        crossOrigin="anonymous"
+      />
+      <div id="container-fdff6bdc16ce487f09160f7bcf039002" />
+      <Script
+        src="https://pl30399277.effectivecpmnetwork.com/16/aa/0b/16aa0b9c9132774934d913c627295656.js"
+        strategy="afterInteractive"
+      />
+    </>
   )
 }
